0385cb3b2d5fd1faf107c9662bf6ab3487ee9cc8
Ilkin Ismayilov <ismayilovilkin2006@gmail.com>
2026-04-15 16:21:19 +0400

    Security and Authorization Exceptions added

---
+++ src/main/java/com/example/exception/SuperAdminAccessDeniedException.java
@@ -0,0 +1,7 @@
+package com.example.exception;
+
+public class SuperAdminAccessDeniedException extends RuntimeException {
+    public SuperAdminAccessDeniedException(String message) {
+        super(message);
+    }
+}
